9cc5a583bcba5353ba60a1d6b1d51cb41e11a289
xcc3641 <446022570@qq.com>
2020-08-26 14:30:42 +0800

    feat: user SelectableText in detail page

--- lib/ui/widget/alice_base_call_details_widget.dart
+++ lib/ui/widget/alice_base_call_details_widget.dart
@@ -4,8 +4,7 @@ import 'package:alice/helper/alice_conversion_helper.dart';
 import 'package:alice/ui/utils/alice_parser.dart';
 import 'package:flutter/material.dart';
 
-abstract class AliceBaseCallDetailsWidgetState<T extends StatefulWidget>
-    extends State<T> {
+abstract class AliceBaseCallDetailsWidgetState<T extends StatefulWidget> extends State<T> {
   final JsonEncoder encoder = new JsonEncoder.withIndent('  ');
 
   Widget getListRow(String name, String value) {
@@ -19,10 +18,7 @@ abstract class AliceBaseCallDetailsWidgetState<T extends StatefulWidget>
         Padding(padding: EdgeInsets.only(left: 5)),
         Flexible(
           fit: FlexFit.loose,
-          child: Text(
-            value,
-            overflow: TextOverflow.clip,
-          ),
+          child: SelectableText(value),
         ),
         Padding(
           padding: EdgeInsets.only(bottom: 18),
@@ -33,12 +29,9 @@ abstract class AliceBaseCallDetailsWidgetState<T extends StatefulWidget>
 
   String formatBytes(int bytes) => AliceConversionHelper.formatBytes(bytes);
 
-  String formatDuration(int duration) =>
-      AliceConversionHelper.formatTime(duration);
+  String formatDuration(int duration) => AliceConversionHelper.formatTime(duration);
 
-  String formatBody(dynamic body, String contentType) =>
-      AliceParser.formatBody(body, contentType);
+  String formatBody(dynamic body, String contentType) => AliceParser.formatBody(body, contentType);
 
-  String getContentType(Map<String, dynamic> headers) =>
-      AliceParser.getContentType(headers);
+  String getContentType(Map<String, dynamic> headers) => AliceParser.getContentType(headers);
 }
