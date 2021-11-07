29be6de2cc9a7fa108a912bdb9b74d8646a9c4df
Igor Kurek <ikurek@users.noreply.github.com>
2021-11-07 12:17:25 +0100

    Updated flutter_local_notifications dependency to support Android 12 (#95)
    Co-authored-by: Jakub <jhomlala@gmail.com>

--- lib/core/alice_core.dart
+++ lib/core/alice_core.dart
@@ -198,7 +198,8 @@ class AliceCore {
     const channelName = "Alice";
     const channelDescription = "Alice";
     final androidPlatformChannelSpecifics = AndroidNotificationDetails(
-        channelId, channelName, channelDescription,
+        channelId, channelName,
+        channelDescription: channelDescription,
         enableVibration: false,
         playSound: false,
         largeIcon: DrawableResourceAndroidBitmap(notificationIcon));
