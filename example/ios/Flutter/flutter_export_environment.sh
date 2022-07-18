e6f09b0af8ecb8b32374f225977261bdcc724f66
Aayush Kedawat <aayushkedawat@gmail.com>
2022-07-18 22:12:35 +0530

    Dependencies update

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/aayushkedawat/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/aayushkedawat/Downloads/Flutter Projects/Flutter Projects/packages/alice/alice/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=true"
+export "PACKAGE_CONFIG=/Users/aayushkedawat/Downloads/Flutter Projects/Flutter Projects/packages/alice/alice/example/.dart_tool/package_config.json"
