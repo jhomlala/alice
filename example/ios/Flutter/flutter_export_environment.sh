002f6811e98a17cfe5c3e48f5064e202c704d257
Shubham Soni <shubham@threedots.app>
2022-08-17 18:55:16 +0530

    dependency upgrade

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/shubham/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/shubham/Desktop/Projects/alice/example"
+export "FLUTTER_ROOT=/Users/shubhamsoni/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/shubhamsoni/Desktop/Dev/alice/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
@@ -10,4 +10,4 @@ export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.packages"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
