e4a9d5cdd49c228f2c236dc8f45103b62964c5c1
Pratama Ramadhan <theramrama@gmail.com>
2022-10-13 09:55:55 +0700

    Change dependency from open_file: ^3.2.1 to open_filex: ^4.1.1

--- lib/helper/alice_save_helper.dart
+++ lib/helper/alice_save_helper.dart
@@ -7,7 +7,7 @@ import 'package:alice/helper/alice_conversion_helper.dart';
 import 'package:alice/model/alice_http_call.dart';
 import 'package:alice/utils/alice_parser.dart';
 import 'package:flutter/material.dart';
-import 'package:open_file/open_file.dart';
+import 'package:open_filex/open_filex.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -92,7 +92,8 @@ class AliceSaveHelper {
           "Success",
           "Successfully saved logs in ${file.path}",
           secondButtonTitle: isAndroid ? "View file" : null,
-          secondButtonAction: () => isAndroid ? OpenFile.open(file.path) : null,
+          secondButtonAction: () =>
+              isAndroid ? OpenFilex.open(file.path) : null,
           brightness: brightness,
         );
         return file.path;
