7449ffd32916e9cedb6ee4a6ac3f7ba5fbb144db
Jakub <jhomlala@gmail.com>
2023-11-10 13:03:17 +0100

    feat: updated dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.4.0
+* [BREAKING_CHANGE] Updated dart min version to 3.0.0
+* Updated dependencies
+
 # 0.3.3
 * Updated dependencies
 * Added fix for call time for http package (by itsara-odds https://github.com/itsara-odds)
--- lib/core/alice_chopper_response_interceptor.dart
+++ lib/core/alice_chopper_response_interceptor.dart
@@ -10,8 +10,8 @@ import 'package:http/http.dart';
 
 import 'alice_core.dart';
 
-class AliceChopperInterceptor extends chopper.ResponseInterceptor
-    with chopper.RequestInterceptor {
+class AliceChopperInterceptor
+    implements chopper.ResponseInterceptor, chopper.RequestInterceptor {
   /// AliceCore instance
   final AliceCore aliceCore;
 
