a87999b191e7c3d5eed39ee5abbf09dd153ad802
Jakub  Homlala <jhomlala@gmail.com>
2024-06-28 14:55:48 +0200

    feat: updated translations

--- packages/alice/lib/ui/common/alice_context_ext.dart
+++ packages/alice/lib/ui/common/alice_context_ext.dart
@@ -1,12 +1,15 @@
-  import 'package:alice/model/alice_translation.dart';
+  import 'dart:io';
+
+import 'package:alice/model/alice_translation.dart';
 import 'package:alice/core/alice_translations.dart';
 import 'package:flutter/material.dart';
 
 extension AliceContextExt on BuildContext{
   String i18n(AliceTranslationKey key){
     try{
-      final locale = Localizations.localeOf(this);
-      final languageCode = locale.languageCode;
+      final locale =Platform.localeName;
+
+      final languageCode = locale.split("_").first;
       return AliceTranslations.get(languageCode: languageCode, key: key);
 
     } catch (error){
