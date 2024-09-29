55d874058bed3a513d02badfbde2140248538008
Jakub Homlala <1329033+jhomlala@users.noreply.github.com>
2024-09-29 15:50:07 +0200

    feat: Chopper Adapter tests (#225)
    * feat: added chopper tests
    
    * feat: added chopper tests
    
    * feat: added chopper tests
    
    * feat: added chopper tests
    
    * feat: added chopper tests
    
    * fix: fixed issues

---
+++ packages/alice_chopper/test/invalid_model.g.dart
@@ -0,0 +1,16 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'invalid_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+InvalidModel _$InvalidModelFromJson(Map<String, dynamic> json) => InvalidModel(
+      id: json['id'] as String?,
+    );
+
+Map<String, dynamic> _$InvalidModelToJson(InvalidModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+    };
