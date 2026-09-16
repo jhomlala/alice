597d589851c4ccad607a8391230e9e3367235c3c
Jakub  Homlala <jhomlala@gmail.com>
2026-09-16 14:21:03 +0200

    feat: move replay logic to separate class for Wasm compatibility

---
+++ packages/alice/lib/helper/alice_replay_helper_stub.dart
@@ -0,0 +1,4 @@
+import 'package:alice/core/alice_core.dart';
+import 'package:alice/helper/alice_replay_helper.dart';
+
+AliceReplayHelper getReplayHelper(AliceCore core) => throw UnimplementedError('Not supported on this platform');
---
+++ packages/alice/lib/helper/alice_replay_helper_web.dart
@@ -0,0 +1,25 @@
+import 'package:alice/core/alice_core.dart';
+import 'package:alice/helper/alice_replay_helper.dart';
+import 'package:alice/model/alice_http_call.dart';
+import 'package:alice/ui/common/alice_dialog.dart';
+import 'package:flutter/material.dart';
+
+AliceReplayHelper getReplayHelper(AliceCore core) => AliceReplayHelperImpl(core);
+
+class AliceReplayHelperImpl implements AliceReplayHelper {
+  final AliceCore core;
+
+  AliceReplayHelperImpl(this.core);
+
+  @override
+  Future<void> replayCall({
+    required AliceHttpCall originalCall,
+    required BuildContext context,
+  }) async {
+    AliceGeneralDialog.show(
+      context: context,
+      title: 'Error',
+      description: 'Replay is not supported on this platform.',
+    );
+  }
+}
